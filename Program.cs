c96b6fc3dd3a7816916fbf7879c80966aa7cbfad
rdanilowicz <rjdanilowicz@gmail.com>
2024-09-21 13:18:04 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Shape_Area_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
